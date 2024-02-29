14d9818ec361fc14b99ecd3aa89f9674363f127f
Rebecca McKenzie <63881668+RMcKenzie111@users.noreply.github.com>
2024-02-29 03:11:55 +0000

    ascii

--- my_solution.rb
+++ my_solution.rb
@@ -1,15 +1,16 @@
 require "http"
 require "json"
+require "ascii_charts"
 
 # Ask for user location
-puts "Hello, whaat's your location?"
+puts "Hello, what's your location?"
 
 # Get and store user location
 user_location = gets.chomp
 
 # Get user's latitude and longitude from Google API
 puts "Let's see what the weather is in #{user_location}"
- gmaps = ENV.fetch("GMAPS-KEY")
+ gmaps = ENV.fetch("GMAPS_KEY")
  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=#{gmaps}"
  
  raw_gmaps = HTTP.get(gmaps_url)
@@ -38,4 +39,40 @@ puts "Let's see what the weather is in #{user_location}"
 # Display the current temperature and summary of the weather for the next hour.
 
 minute_hash = parsed_pw.fetch("minuteley", false)
-if
+if minute_hash
+  weather_summary = minute_hash.fetch("summary")
+  puts "Next hour #{weather_summary}"
+end
+
+#For each of the next twelve hours, check if the precipitation probability is greater than 10%.
+
+hour_hash = parsed_pw.fetch("hourly")
+hour_array = hour_hash.fetch("data")
+next_twelve = hour_array[1..12]
+precip_threshold = 0.10
+any_precip = false
+
+next_twelve.each do |the_hour|
+  precip_probability = the_hour.fetch("precipProbability")
+
+  #If so, print a message saying how many hours from now and what the precipitation probability is.
+
+  if precip_probability > precip_threshold
+    any_precip = true
+    precip_time = Time.at(the_hour.fetch("time"))
+    seconds_later = precip_time - Time.now
+    hours_later = seconds_later / 60 / 60
+    puts "In #{hours_later.round} hours, there is a #{(precip_probability * 100).round}% chance of precipitation."
+  end
+  if any_precip == true
+    puts "You might want to carry an umbrella!"
+    puts AsciiCharts::Cartesian.new({hours_later.round => (precip_probability * 100).round}, :bar => true, :hide_zero => true).draw
+  end
+end
+
+#If any of the next twelve hours has a precipitation probability greater than 10%, print “You might want to carry an umbrella!”
+#If not, print “You probably won’t need an umbrella today.”
+  if any_precip == false
+    puts "You probaly won't need an umbrella today."
+  end
+    
